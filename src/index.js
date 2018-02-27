24c78d9ae8ed590b381e2161555093ec6c6ff2eb
vvsvintsitsky <v.v.svintsitsky@gmail.com>
2018-02-27 16:48:13 +0300

    counting consecutive sequence in tree

--- src/index.js
+++ src/index.js
@@ -16,21 +16,45 @@ module.exports = function longestConsecutiveLength(array) {
   }
 
   var tree = {p : null, l : null, r : null, v : null};
-  array = [100,4,200,1,3,2];
+  //array = [100,4,200,1,3,2];
   tree.v = array[0];
   var prevValue;
   var consecutiveLength = 0;
-  var info = {prev: null, consLength : 0};
+  var info = {prev: null, currentConsLength : 0, maxConsLength : 0};
   for(var i = 1; i < array.length; i++) {
     insertNode(tree, array[i]);
   }
 
-  var sortedArray = [];
+  var countConsecutiveLength = function(node, info) {
+    if(node.l != null) {
+      countConsecutiveLength(node.l, info);
+    }
+    
+    if((node.v - 1) !== info.prev) {
+      if(info.currentConsLength > info.maxConsLength) {
+        info.maxConsLength = info.currentConsLength;
+      }
+      info.currentConsLength = 1;
+    } else {
+      info.currentConsLength++;
+    }
+    info.prev = node.v;
+    
+    if(node.r != null) {
+      countConsecutiveLength(node.r, info);
+    }
+  }
+
+  countConsecutiveLength(tree, info);
+  console.log(info.maxConsLength);
+
+  /*var sortedArray = [];
   var fillArrayFromTree = function(node, arrayToFill) {
     if(node.l != null) {
       fillArrayFromTree(node.l, arrayToFill);
     }
     arrayToFill.push(node.v);
+    
     if(node.r != null) {
       fillArrayFromTree(node.r, arrayToFill);
     }
@@ -38,5 +62,19 @@ module.exports = function longestConsecutiveLength(array) {
 
   fillArrayFromTree(tree, sortedArray);
 
-  console.log(sortedArray);
+  info.prev = sortedArray[0];
+  for(var i = 1; i < sortedArray.length; i++) {
+    if((sortedArray[i] - 1) !== info.prev) {
+      console.log(info.prev + " !=> " + sortedArray[i]);
+      if(info.currentConsLength > info.maxConsLength) {
+        info.maxConsLength = info.currentConsLength;
+      }
+      info.currentConsLength = 0;
+    } else {
+      info.currentConsLength++;
+    }
+    info.prev = sortedArray[i];
+  }
+
+  console.log(info.maxConsLength);*/
 }
