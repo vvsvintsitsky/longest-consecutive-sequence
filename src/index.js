12b20f0ec88efcab18341f78e194f8357b502712
vvsvintsitsky <v.v.svintsitsky@gmail.com>
2018-02-28 09:19:56 +0300

    completed longest sequesce with 23s result

--- src/index.js
+++ src/index.js
@@ -3,8 +3,26 @@ module.exports = function longestConsecutiveLength(array) {
   if(array.length == 0) {
     return 0;
   }
+  var info = {current: null, currentConsLength : 1, maxConsLength : 1};
 
-  var insertNode = function(node, value) {
+  /*while(set.size != 0) {
+    while(set.has(currentNumber)) {
+      info.currentConsLength++;
+      set.delete(currentNumber--);
+    }
+    currentNumber = info.prev + 1;
+    while(set.has(currentNumber)) {
+      info.currentConsLength++;
+      set.delete(currentNumber++);
+    }
+    if(info.currentConsLength > info.maxConsLength) {
+      info.maxConsLength = info.currentConsLength;
+    }
+    info.currentConsLength = 1;
+    //current = set.
+  }*/
+
+  /*var insertNode = function(node, value) {
     if(value <= node.v) {
       if(node.l == null) {
         node.l = {p : node, l : null, r : null, v : value};
@@ -23,7 +41,7 @@ module.exports = function longestConsecutiveLength(array) {
   var tree = {p : null, l : null, r : null, v : null};
   //array = [100,4,200,1,3,2];
   tree.v = array[0];
-  var info = {prev: null, currentConsLength : 1, maxConsLength : 1};
+  
   for(var i = 1; i < array.length; i++) {
     insertNode(tree, array[i]);
   }
@@ -33,23 +51,31 @@ module.exports = function longestConsecutiveLength(array) {
       countMaxConsecutiveLength(node.l, info);
     }
     
-    if((node.v - 1) !== info.prev) {
-      if(info.currentConsLength > info.maxConsLength) {
-        info.maxConsLength = info.currentConsLength;
+    if(node.v != info.prev) {
+      if((node.v - 1) !== info.prev) {
+        if(info.currentConsLength > info.maxConsLength) {
+          info.maxConsLength = info.currentConsLength;
+        }
+        info.currentConsLength = 1;
+      } else {
+        info.currentConsLength++;
       }
-      info.currentConsLength = 1;
-    } else {
-      info.currentConsLength++;
+      info.prev = node.v;
     }
-    info.prev = node.v;
-    
+        
     if(node.r != null) {
       countMaxConsecutiveLength(node.r, info);
     }
   }
 
   countMaxConsecutiveLength(tree, info);
-  return info.maxConsLength;
+  return info.maxConsLength;*/
+
+
+
+
+
+
   //console.log(info.maxConsLength);
 
   /*var sortedArray = [];
@@ -81,4 +107,39 @@ module.exports = function longestConsecutiveLength(array) {
   }
 
   console.log(info.maxConsLength);*/
+
+  var set = new Set(array);
+  //for (let item of set.values()) console.log(item);
+
+  var countConsLengthWithSet = function(set, info) {
+    for (let item of set.values()) {
+      var currentNumber;
+      info.current = item;
+      currentNumber = info.current - 1;
+      set.delete(item)
+      while(set.has(currentNumber)) {
+        info.currentConsLength++;
+        set.delete(currentNumber--);
+      }
+      currentNumber = info.current + 1;
+      while(set.has(currentNumber)) {
+        info.currentConsLength++;
+        set.delete(currentNumber++);
+      }
+      if(info.currentConsLength > info.maxConsLength) {
+        info.maxConsLength = info.currentConsLength;
+      }
+      info.currentConsLength = 1;
+      break;
+    }
+  }
+
+  while(set.size != 0) {
+    //console.log(set.length);
+    countConsLengthWithSet(set, info);
+    
+  }
+  return info.maxConsLength;
+
+  
 }
